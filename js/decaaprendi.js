54e2fffc2e9c71f175944cc574a31388823c256c
Alejandro Andrade <alejandro21112@hotmail.com>
2023-12-02 22:41:23 -0500

    solucionado el tema de las cartas dentro de CUBE

--- js/decaaprendi.js
+++ js/decaaprendi.js
@@ -5,14 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
   const opcion1 = document.getElementById('CUBE');
   const opcion2 = document.getElementById('Cestas');
   const opcion3 = document.getElementById('Básicos-de-la-sección');
-  const tarjetasCube = document.querySelectorAll('.tarjeta-apartado-cube');
+  const tarjetaDashboard = document.getElementById('apartado-cube-dashboard');
+  const tarjetaComercio = document.getElementById('apartado-cube-comercio');
+  const tarjetaCatalogo = document.getElementById('apartado-cube-catalogo');
+  const tarjetaGestionUsuario = document.getElementById('apartado-cube-gestion-usuario');
+  const tarjetaCesta = document.getElementById('apartado-cube-cesta');
+  const tarjetaPedido = document.getElementById('apartado-cube-pedido');
+  const tarjetaFlujoMercancias = document.getElementById('apartado-cube-flujo-mercancias');
+  const tarjetaCestaImpresion = document.getElementById('apartado-cube-cesta-impresion');
+  const tarjetaGestionBackoffice = document.getElementById('apartado-cube-gestion-backoffice');
   const seccionesContenido = document.querySelectorAll('section[id^="contenido-"]');
 
-  // Variable para almacenar la sección seleccionada en CUBE
-  let seccionSeleccionadaCube = null;
-
   // Función para manejar el clic en las tarjetas de aprendizaje
-  function handleTarjetaClick(tarjeta) {
+  function handleTarjetaClick(tarjeta, seccionId) {
     // Remover la clase 'selected' de todas las tarjetas
     tarjetas.forEach(t => t.classList.remove('selected'));
 
@@ -20,20 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
     tarjeta.classList.add('selected');
 
     // Mostrar la sección correspondiente al hacer clic en la tarjeta
-    const seccionId = tarjeta.getAttribute('for');
     showSection(seccionId);
-  }
-
-  // Función para manejar el clic en las tarjetas de apartados en CUBE
-  function handleTarjetasCubeClick(tarjeta) {
-    // Remover la clase 'selected' de todas las tarjetas Cube
-    tarjetasCube.forEach(t => t.classList.remove('selected'));
-
-    // Agregar la clase 'selected' solo a la tarjeta clicada
-    tarjeta.classList.add('selected');
-
-    // Almacenar la sección correspondiente al hacer clic en la tarjeta
-    seccionSeleccionadaCube = tarjeta.getAttribute('for');
   }
 
   // Función para mostrar la sección correspondiente
@@ -46,7 +38,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // Agregar evento de clic a cada tarjeta de aprendizaje
   tarjetas.forEach(tarjeta => {
     tarjeta.addEventListener('click', function () {
-      handleTarjetaClick(tarjeta);
+      const seccionId = tarjeta.getAttribute('for');
+      handleTarjetaClick(tarjeta, seccionId);
     });
 
     tarjeta.addEventListener('mouseenter', function () {
@@ -56,6 +49,51 @@ document.addEventListener('DOMContentLoaded', function () {
     tarjeta.addEventListener('mouseleave', function () {
       tarjeta.classList.remove('hover-effect');
     });
+  });
+
+  // Agregar evento de clic a la tarjeta de Dashboard
+  tarjetaDashboard.addEventListener('click', function () {
+    showSection('dashboard');
+  });
+
+  // Agregar evento de clic a la tarjeta de Comercio
+  tarjetaComercio.addEventListener('click', function () {
+    showSection('comercio');
+  });
+
+  // Agregar evento de clic a la tarjeta de Catalogo
+  tarjetaCatalogo.addEventListener('click', function () {
+    showSection('catalogo');
+  });
+
+  // Agregar evento de clic a la tarjeta de Gestion Usuario
+  tarjetaGestionUsuario.addEventListener('click', function () {
+    showSection('gestion-del-usuario');
+  });
+
+  // Agregar evento de clic a la tarjeta de Cesta
+  tarjetaCesta.addEventListener('click', function () {
+    showSection('cesta');
+  });
+
+  // Agregar evento de clic a la tarjeta de Pedido
+  tarjetaPedido.addEventListener('click', function () {
+    showSection('pedido');
+  });
+
+  // Agregar evento de clic a la tarjeta de Flujo Mercancias
+  tarjetaFlujoMercancias.addEventListener('click', function () {
+    showSection('flujo-mercancias');
+  });
+
+  // Agregar evento de clic a la tarjeta de Cesta Impresion
+  tarjetaCestaImpresion.addEventListener('click', function () {
+    showSection('cesta-impresion');
+  });
+
+  // Agregar evento de clic a la tarjeta de Gestion Backoffice
+  tarjetaGestionBackoffice.addEventListener('click', function () {
+    showSection('gestion-backoffice');
   });
 
   // Agregar evento de clic al botón de seleccionar
@@ -72,64 +110,4 @@ document.addEventListener('DOMContentLoaded', function () {
       showSection('basicos');
     }
   });
-
-// Agregar evento de clic a cada tarjeta Cube
-tarjetasCube.forEach(tarjetaCube => {
-  tarjetaCube.addEventListener('click', function () {
-      handleTarjetasCubeClick(tarjetaCube);
-  });
-
-  tarjetaCube.addEventListener('mouseenter', function () {
-      tarjetaCube.classList.add('hover-effect');
-  });
-
-  tarjetaCube.addEventListener('mouseleave', function () {
-      tarjetaCube.classList.remove('hover-effect');
-  });
-});
-
-// Agregar eventos de clic para los elementos del dashboard y del apartado CUBE
-const tarjetaDashboard = document.querySelector('.tarjeta-apartado-cube[for="apartado-cube-dashboard"]');
-tarjetaDashboard.addEventListener('click', function () {
-  // Al hacer clic en el dashboard, mostrar su sección correspondiente
-  showSection('contenido-dashboard');
-  // Asignar la sección seleccionada al dashboard
-  seccionSeleccionadaCube = 'contenido-dashboard';
-});
-
-// Obtener el botón de seleccionar de CUBE
-const botonSeleccionarCube = document.getElementById('boton-seleccionar-CUBE');
-botonSeleccionarCube.addEventListener('click', function () {
-  // Mostrar la sección correspondiente a la opción seleccionada en CUBE
-  showSection(seccionSeleccionadaCube);
-});
-
-// Función para manejar el clic en las tarjetas de apartados en CUBE
-function handleTarjetasCubeClick(tarjeta) {
-  // Remover la clase 'selected' de todas las tarjetas Cube
-  tarjetasCube.forEach(t => t.classList.remove('selected'));
-
-  // Agregar la clase 'selected' solo a la tarjeta clicada
-  tarjeta.classList.add('selected');
-
-  // Almacenar la sección correspondiente al hacer clic en la tarjeta
-  seccionSeleccionadaCube = tarjeta.getAttribute('for');
-}
-
-const botonSeleccionarDashboard = document.getElementById('boton-seleccionar-dashboard'); // Declare this variable before using it
-
-botonSeleccionarDashboard.addEventListener('click', function () {
-  showSection('contenido-dashboard');
-  seccionSeleccionadaCube = 'contenido-dashboard';
-});
-botonSeleccionarCube.addEventListener('click', function () {
-  if (seccionSeleccionadaCube) {
-    showSection(seccionSeleccionadaCube);
-  } else {
-    // Set a default section if seccionSeleccionadaCube is not defined
-    showSection('default-section');
-  }
-});
-
-
 });
