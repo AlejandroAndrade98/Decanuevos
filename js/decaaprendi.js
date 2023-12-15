924a8f9035c04eafa4048b5b540e195b81719512
Alejandro Andrade <alejandro21112@hotmail.com>
2023-12-15 13:09:10 -0500

    solucionado el problema de las imagenes en contenido ocultable, pero ahora necesito buscar la forma de ocultar el resto de contenido

--- js/decaaprendi.js
+++ js/decaaprendi.js
@@ -16,57 +16,26 @@ document.addEventListener('DOMContentLoaded', function () {
   const tarjetaGestionBackoffice = document.getElementById('apartado-cube-gestion-backoffice');
   const seccionesContenido = document.querySelectorAll('section[id^="contenido-"]');
   const botonesInfo = document.querySelectorAll('.tarjeta-Dashboard button');
- 
 
+// Función para manejar el clic en los botones de retroceso
+function handleInicioClick() {
+  // Remover la clase 'selected' de todas las tarjetas
+  tarjetas.forEach(t => t.classList.remove('selected'));
 
-   let seleccionActual = null;
-
-  // Función para manejar el clic en las tarjetas de aprendizaje
-  function handleTarjetaClick(tarjeta, seccionId) {
-    // Ocultar todas las secciones
-    seccionesContenido.forEach(seccion => {
-      seccion.style.display = 'none';
-    });
-
-    // Mostrar la sección correspondiente al hacer clic en la tarjeta
-    document.getElementById(`contenido-${seccionId}`).style.display = 'flex';
-
-    // Actualizar la selección actualatrás
-    seleccionActual = seccionId;
-  }
-
-  // Agregar evento de clic a cada tarjeta de aprendizaje
-  tarjetas.forEach(tarjeta => {
-    tarjeta.addEventListener('click', function () {
-      const seccionId = tarjeta.getAttribute('for');
-      handleTarjetaClick(tarjeta, seccionId);
-    });
+  // Ocultar todas las secciones
+  seccionesContenido.forEach(seccion => {
+    seccion.style.display = 'none';
   });
 
-  // Agregar evento de clic al botón de seleccionar
-  botonSeleccionar.addEventListener('click', function () {
-    // Ocultar el elemento de selección de aprendizaje
-    seleccionAprendizaje.style.display = 'none';
+  // Mostrar la selección anterior
+  seleccionAprendizaje.style.display = 'flex';
+  seleccionActual = null;
+}
 
-    // En este punto, seleccionActual debería contener la sección seleccionada
-  });
-
-  // Función para manejar el clic en los botones de retroceso
-  function handleInicioClick() {
-    // Ocultar todas las secciones
-    seccionesContenido.forEach(seccion => {
-      seccion.style.display = 'none';
-    });
-
-    // Mostrar la selección anterior
-    seleccionAprendizaje.style.display = 'flex';
-    seleccionActual = null;
-  }
-
-  // Agregar evento de clic a los botones de retroceso
-  document.querySelectorAll('[id^="boton-Inicio"]').forEach(botonInicio => {
-    botonInicio.addEventListener('click', handleInicioClick);
-  });
+// Agregar evento de clic a los botones de retroceso
+document.querySelectorAll('[id^="boton-Inicio"]').forEach(botonInicio => {
+  botonInicio.addEventListener('click', handleInicioClick);
+});
 
   // Función para manejar el clic en las tarjetas de aprendizaje
   function handleTarjetaClick(tarjeta, seccionId) {
@@ -153,45 +122,43 @@ document.addEventListener('DOMContentLoaded', function () {
     // Ocultar el elemento de selección de aprendizaje
     seleccionAprendizaje.style.display = 'none';
 
-     // Agregar evento de clic a los botones de "+ Info" en la sección de Dashboard
-     botonesInfo.forEach(boton => {
-      boton.addEventListener('click', function () {
-        // Encuentra el párrafo asociado al botón
-        const parrafo = boton.previousElementSibling;
-  
-// Obtén todos los botones de información
-const botonesInfo = document.querySelectorAll('.boton-info');
-
-// Agregar evento de clic a los botones de "+ Info"
-botonesInfo.forEach(boton => {
-  boton.addEventListener('click', function () {
-    // Encuentra el contenedor padre
-    const contenedor = boton.closest('.carta');
-
-    // Encuentra el párrafo y la imagen dentro del contenedor
-    const parrafo = contenedor.querySelector('p');
-    const imagen = contenedor.querySelector('.contenido-imagen');
-
-    // Alternar la visibilidad del párrafo e imagen
-    parrafo.style.display = (parrafo.style.display === 'none') ? 'block' : 'none';
-    imagen.style.display = (imagen.style.display === 'none') ? 'block' : 'none';
+    // Verificar qué opción ha sido seleccionada y mostrar el contenido correspondiente
+    if (opcion1.checked) {
+      showSection('cube');
+    } else if (opcion2.checked) {
+      showSection('cestas');
+    } else if (opcion3.checked) {
+      showSection('basicos');
+    }
   });
-});
-        
-      });
-      
-      
 
-    })
-      // Verificar qué opción ha sido seleccionada y mostrar el contenido correspondiente
-      if (opcion1.checked) {
-        showSection('cube');
-      } else if (opcion2.checked) {
-        showSection('cestas');
-      } else if (opcion3.checked) {
-        showSection('basicos');
+  // Agregar evento de clic a los botones de "+ Info" en la sección de Dashboard
+  botonesInfo.forEach(boton => {
+    boton.addEventListener('click', function () {
+      // Encuentra el párrafo asociado al botón
+      const parrafo = boton.previousElementSibling;
+      // Haz lo que necesitas con el párrafo
+    });
+  });
+
+  // Obtén todos los botones de información
+  const botonesInfoSeccion = document.querySelectorAll('.boton-info');
+
+  // Agregar evento de clic a los botones de "+ Info"
+  botonesInfoSeccion.forEach(boton => {
+    boton.addEventListener('click', function () {
+      // Encuentra el contenedor padre
+      const contenedor = boton.closest('.carta');
+
+      // Encuentra el párrafo y la imagen dentro del contenedor
+      const parrafo = contenedor.querySelector('p');
+      const imagen = contenedor.querySelector('.contenido-imagen');
+
+      // Alternar la visibilidad del párrafo e imagen
+      if (parrafo && imagen) {
+        parrafo.style.display = (parrafo.style.display === 'block') ? 'none' : 'block';
+        imagen.style.display = (imagen.style.display === 'block') ? 'none' : 'block';
       }
     });
-  
-  
   });
+});
